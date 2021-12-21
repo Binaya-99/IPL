3530c252d5d6aa9d3722cb27274fd456b315d31c
Binaya Ray <binaya.ray99@gmail.com>
2021-12-21 15:56:35 +0530

    initial commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ inputBox.onkeyup = (e)=>{
         //     linkTag.setAttribute("href", webLink);
         //     linkTag.click();
         // }
-        emptyArray = suggestions.filter((data)=>{
+        emptyArray = playersName.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
         });
